c7b8cacabb34a1ac065637a059ee7e40c1cc0112
kehinded <80984106+Kehinded@users.noreply.github.com>
2021-06-07 01:54:10 +0100

    Add files via upload

---
+++ medical-appointment.js
@@ -0,0 +1,18 @@
+// ui control
+var uiControl = (function(){
+    return{
+ 
+     
+    }
+ 
+ })();
+ 
+ 
+//  global control
+ var globalControl = (function(uiCtrl){
+  
+ 
+ 
+ 
+ 
+ })(uiControl);
